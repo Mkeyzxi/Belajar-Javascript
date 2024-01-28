551be43d5f7642d89c5000715b652a4f50543d86
Mkeyzxi <98542676+Mkeyzxi@users.noreply.github.com>
2024-01-28 23:10:55 +0800

    Add files via upload

---
+++ Belajar js/js-lanjutan/higher-order-function.js
@@ -0,0 +1,15 @@
+function pengulangan(banyak) {
+    for (let i = 1; i <= banyak; i++) {
+        console.log(i)
+    }
+}
+
+pengulangan(10)
+
+// function pengulangan(banyak, metode) {
+//     for (let i = 0; i < banyak; i++) {
+//         metode(i);
+//     }
+// }
+
+// pengulangan(10, console.log);
