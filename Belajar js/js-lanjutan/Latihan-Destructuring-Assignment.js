649dda6788466ece097223cba3d317c021bc27a1
Mkeyzxi <98542676+Mkeyzxi@users.noreply.github.com>
2024-02-05 03:32:03 +0800

    Update Latihan-Destructuring-Assignment.js

--- Belajar js/js-lanjutan/Latihan-Destructuring-Assignment.js
+++ Belajar js/js-lanjutan/Latihan-Destructuring-Assignment.js
@@ -19,3 +19,7 @@ let saya = {
 // menukar nilai pada objek
 [saya.kelas, saya.nama, saya.semester] = [saya.semester, saya.kelas, saya.nama]
 console.log(saya.nama)
+
+// assigment tanpa deklarasi objek
+({ nama, kelas } = { nama: 'muhammad mbul', kelas: 14 })
+console.log(nama, kelas)
